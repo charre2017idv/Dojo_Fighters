e197ee34d3aa7995194f20a7bfea209def8db4d9
Gustavo Alvarez <idv18c.galvarez@uartesdigitales.edu.mx>
2019-07-10 14:31:36 -0500

    Add movement by force script

---
+++ DOJO-FIGTHER PROTO/Assets/Scripts/MOVEMENT/ForceMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceMovement : MonoBehaviour
+{
+    public Vector2 Forcex;
+    public Vector2 LimitVector;
+    public Vector2 DashVector;
+    public float SpeedLimit;
+    Rigidbody2D MyRB;
+    public float DashTime;
+    float CurrDashTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyRB = GetComponent<Rigidbody2D>();
+        CurrDashTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CurrDashTime += Time.deltaTime;
+        if(Input.GetKey(KeyCode.D))
+        {
+            MyRB.AddForce(Forcex, ForceMode2D.Force);
+            if(MyRB.velocity.x > SpeedLimit)
+            {
+                MyRB.AddForce(-LimitVector, ForceMode2D.Force);
+            }
+            if (Input.GetKey(KeyCode.F))
+            {
+                if (CurrDashTime > DashTime)
+                {
+                    MyRB.AddForce(DashVector, ForceMode2D.Impulse);
+                    CurrDashTime = 0;
+                }
+            }
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            MyRB.AddForce(-Forcex, ForceMode2D.Force);
+            if (MyRB.velocity.x < -SpeedLimit)
+            {
+                MyRB.AddForce(LimitVector, ForceMode2D.Force);
+            }
+            if (Input.GetKey(KeyCode.F))
+            {
+                if (CurrDashTime > DashTime)
+                {
+                    MyRB.AddForce(-DashVector, ForceMode2D.Impulse);
+                    CurrDashTime = 0;
+                }
+            }
+        }
+    }
+}
